c407671d8a8f0811fe2379ada82a48ecbe74d695
Marvin Rabe <marvin.rabe@gmail.com>
2022-02-09 18:13:08 +0100

    feat: add wizard route macro

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,12 +2,20 @@
 
 namespace MarvinRabe\LaravelWizards;
 
+use Illuminate\Support\Facades\Route;
 use MarvinRabe\LaravelWizards\Repositories\SessionWizardRepository;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
-    public function register()
+    public function register(): void
     {
         $this->app->bind(WizardRepository::class, SessionWizardRepository::class);
+    }
+
+    public function boot(): void
+    {
+        Route::macro('wizard', function (string $uri, mixed $action) {
+            return Route::match(['get', 'post'], $uri.'/{id?}/{step?}', $action);
+        });
     }
 }
