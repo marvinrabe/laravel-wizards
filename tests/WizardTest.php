dc1cb2ef4072597c6ac8e69f3e9e2c6a625bddd4
Marvin Rabe <marvin.rabe@gmail.com>
2022-02-22 13:48:11 +0100

    test: add tests for new payload behaviour

--- tests/WizardTest.php
+++ tests/WizardTest.php
@@ -9,8 +9,24 @@ class WizardTest extends TestCase
 {
     public function testIdIsUuid(): void
     {
-        $wizard = new Wizard([]);
+        $wizard = new Wizard();
 
         $this->assertTrue(Str::isUuid($wizard->id));
+    }
+
+    public function testDefaultPayload(): void
+    {
+        $wizard = new Wizard();
+
+        $this->assertNull($wizard->payload);
+    }
+
+    public function testSetPayload(): void
+    {
+        $wizard = new Wizard();
+
+        $wizard->setPayload('::PAYLOAD::');
+
+        $this->assertEquals('::PAYLOAD::', $wizard->payload);
     }
 }
